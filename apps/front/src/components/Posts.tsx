421dda3893db432f4ec2c5a380dbcc69d24d1f90
HysenB <hysi2000@live.com>
2025-03-17 01:34:12 +0100

    postCard component

--- apps/front/src/components/Posts.tsx
+++ apps/front/src/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/lib/types/modelTypes";
+import PostCard from "./postCard";
 
 type Props = {
     posts: Post[]
@@ -11,7 +12,7 @@ const Posts = (props: Props) => {
             </h2>
             <div className="h-1 mx-auto bg-gradient-to-r from-sky-500 to-indigo-500 w-96 mb-9 rounded-t-md mt-5"></div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
-                post here
+                {props.posts.map((post, idx) => <PostCard key={idx} {...post} />)}
             </div>
         </section>
     );
